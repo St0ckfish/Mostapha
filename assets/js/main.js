e54a0e3c617d530cbb970a0423fbe6de466e9249
Mostapha Taha <101586362+M0staphaTaha@users.noreply.github.com>
2023-12-07 17:04:44 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -273,9 +273,21 @@
       mirror: false,
     });
   });
+// **********
 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+// **********
   /**
    * Initiate Pure Counter
    */
   new PureCounter();
 })();
+
